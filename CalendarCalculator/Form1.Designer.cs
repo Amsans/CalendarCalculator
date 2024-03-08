1cf3d7451412aa4626792444a121ead7b0b1de8f
muzykalegion <muzyka.legion@gmail.com>
2024-03-09 00:28:07 +0200

    Add IconTray with app autostart functionality

--- CalendarCalculator/Form1.Designer.cs
+++ CalendarCalculator/Form1.Designer.cs
@@ -39,13 +39,13 @@
             this.TabControl = new System.Windows.Forms.TabControl();
             this.converterTab = new System.Windows.Forms.TabPage();
             this.heliadaTab = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
             this.heliadsInTut = new System.Windows.Forms.Label();
             this.heliadsInCommon = new System.Windows.Forms.Label();
             this.plusLabel = new System.Windows.Forms.Label();
             this.heliadaUpDown = new System.Windows.Forms.NumericUpDown();
             this.HeliadaDatePicker = new System.Windows.Forms.DateTimePicker();
             this.FormTooltip = new System.Windows.Forms.ToolTip(this.components);
-            this.label1 = new System.Windows.Forms.Label();
             this.TabControl.SuspendLayout();
             this.converterTab.SuspendLayout();
             this.heliadaTab.SuspendLayout();
@@ -55,19 +55,21 @@
             // DatePicker
             // 
             this.DatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.DatePicker.Location = new System.Drawing.Point(34, 31);
+            this.DatePicker.Location = new System.Drawing.Point(51, 48);
+            this.DatePicker.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.DatePicker.MinDate = new System.DateTime(1996, 3, 1, 0, 0, 0, 0);
             this.DatePicker.Name = "DatePicker";
-            this.DatePicker.Size = new System.Drawing.Size(100, 20);
+            this.DatePicker.Size = new System.Drawing.Size(148, 26);
             this.DatePicker.TabIndex = 0;
             this.DatePicker.Value = new System.DateTime(2020, 6, 10, 0, 0, 0, 0);
             this.DatePicker.ValueChanged += new System.EventHandler(this.DatePicker_ValueChanged);
             // 
             // ToCommonButton
             // 
-            this.ToCommonButton.Location = new System.Drawing.Point(104, 83);
+            this.ToCommonButton.Location = new System.Drawing.Point(156, 128);
+            this.ToCommonButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ToCommonButton.Name = "ToCommonButton";
-            this.ToCommonButton.Size = new System.Drawing.Size(30, 20);
+            this.ToCommonButton.Size = new System.Drawing.Size(45, 31);
             this.ToCommonButton.TabIndex = 3;
             this.ToCommonButton.Text = ">";
             this.ToCommonButton.UseVisualStyleBackColor = true;
@@ -76,26 +78,29 @@
             // CommonFormatLabel
             // 
             this.CommonFormatLabel.AutoSize = true;
-            this.CommonFormatLabel.Location = new System.Drawing.Point(146, 86);
+            this.CommonFormatLabel.Location = new System.Drawing.Point(219, 132);
+            this.CommonFormatLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CommonFormatLabel.Name = "CommonFormatLabel";
-            this.CommonFormatLabel.Size = new System.Drawing.Size(105, 13);
+            this.CommonFormatLabel.Size = new System.Drawing.Size(160, 20);
             this.CommonFormatLabel.TabIndex = 4;
             this.CommonFormatLabel.Text = "commonFormatLabel";
             this.FormTooltip.SetToolTip(this.CommonFormatLabel, "Двойной клик по этой дате скопирует её в буфер обмена");
             // 
             // TutFormatInput
             // 
-            this.TutFormatInput.Location = new System.Drawing.Point(34, 83);
+            this.TutFormatInput.Location = new System.Drawing.Point(51, 128);
+            this.TutFormatInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TutFormatInput.Name = "TutFormatInput";
-            this.TutFormatInput.Size = new System.Drawing.Size(70, 20);
+            this.TutFormatInput.Size = new System.Drawing.Size(103, 26);
             this.TutFormatInput.TabIndex = 5;
             // 
             // TutFormatLabel
             // 
             this.TutFormatLabel.AutoSize = true;
-            this.TutFormatLabel.Location = new System.Drawing.Point(146, 37);
+            this.TutFormatLabel.Location = new System.Drawing.Point(219, 57);
+            this.TutFormatLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TutFormatLabel.Name = "TutFormatLabel";
-            this.TutFormatLabel.Size = new System.Drawing.Size(77, 13);
+            this.TutFormatLabel.Size = new System.Drawing.Size(118, 20);
             this.TutFormatLabel.TabIndex = 1;
             this.TutFormatLabel.Text = "tutFormatLabel";
             this.FormTooltip.SetToolTip(this.TutFormatLabel, "Двойной клик по этой дате скопирует её в буфер обмена");
@@ -104,9 +109,10 @@
             // 
             this.TutInputValidation.AutoSize = true;
             this.TutInputValidation.ForeColor = System.Drawing.Color.Red;
-            this.TutInputValidation.Location = new System.Drawing.Point(31, 106);
+            this.TutInputValidation.Location = new System.Drawing.Point(46, 163);
+            this.TutInputValidation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TutInputValidation.Name = "TutInputValidation";
-            this.TutInputValidation.Size = new System.Drawing.Size(101, 13);
+            this.TutInputValidation.Size = new System.Drawing.Size(152, 20);
             this.TutInputValidation.TabIndex = 6;
             this.TutInputValidation.Text = "Неверный формат";
             this.FormTooltip.SetToolTip(this.TutInputValidation, "Формат даты должен быть I.II.IV.X");
@@ -116,10 +122,11 @@
             // 
             this.TabControl.Controls.Add(this.converterTab);
             this.TabControl.Controls.Add(this.heliadaTab);
-            this.TabControl.Location = new System.Drawing.Point(1, 7);
+            this.TabControl.Location = new System.Drawing.Point(2, 11);
+            this.TabControl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TabControl.Name = "TabControl";
             this.TabControl.SelectedIndex = 0;
-            this.TabControl.Size = new System.Drawing.Size(313, 188);
+            this.TabControl.Size = new System.Drawing.Size(470, 289);
             this.TabControl.TabIndex = 7;
             // 
             // converterTab
@@ -130,10 +137,11 @@
             this.converterTab.Controls.Add(this.TutFormatInput);
             this.converterTab.Controls.Add(this.ToCommonButton);
             this.converterTab.Controls.Add(this.CommonFormatLabel);
-            this.converterTab.Location = new System.Drawing.Point(4, 22);
+            this.converterTab.Location = new System.Drawing.Point(4, 29);
+            this.converterTab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.converterTab.Name = "converterTab";
-            this.converterTab.Padding = new System.Windows.Forms.Padding(3);
-            this.converterTab.Size = new System.Drawing.Size(284, 162);
+            this.converterTab.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.converterTab.Size = new System.Drawing.Size(462, 256);
             this.converterTab.TabIndex = 0;
             this.converterTab.Text = "Конвертер";
             // 
@@ -145,43 +153,58 @@
             this.heliadaTab.Controls.Add(this.plusLabel);
             this.heliadaTab.Controls.Add(this.heliadaUpDown);
             this.heliadaTab.Controls.Add(this.HeliadaDatePicker);
-            this.heliadaTab.Location = new System.Drawing.Point(4, 22);
+            this.heliadaTab.Location = new System.Drawing.Point(4, 29);
+            this.heliadaTab.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.heliadaTab.Name = "heliadaTab";
-            this.heliadaTab.Padding = new System.Windows.Forms.Padding(3);
-            this.heliadaTab.Size = new System.Drawing.Size(305, 162);
+            this.heliadaTab.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.heliadaTab.Size = new System.Drawing.Size(462, 256);
             this.heliadaTab.TabIndex = 1;
             this.heliadaTab.Text = "Хелиады";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(435, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Подсчёт N-ой хелиады (1000 дней) для указанной даты";
             // 
             // heliadsInTut
             // 
             this.heliadsInTut.AutoSize = true;
-            this.heliadsInTut.Location = new System.Drawing.Point(203, 86);
+            this.heliadsInTut.Location = new System.Drawing.Point(304, 132);
+            this.heliadsInTut.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.heliadsInTut.Name = "heliadsInTut";
-            this.heliadsInTut.Size = new System.Drawing.Size(35, 13);
+            this.heliadsInTut.Size = new System.Drawing.Size(51, 20);
             this.heliadsInTut.TabIndex = 4;
             this.heliadsInTut.Text = "label2";
             // 
             // heliadsInCommon
             // 
             this.heliadsInCommon.AutoSize = true;
-            this.heliadsInCommon.Location = new System.Drawing.Point(199, 61);
+            this.heliadsInCommon.Location = new System.Drawing.Point(298, 94);
+            this.heliadsInCommon.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.heliadsInCommon.Name = "heliadsInCommon";
-            this.heliadsInCommon.Size = new System.Drawing.Size(35, 13);
+            this.heliadsInCommon.Size = new System.Drawing.Size(51, 20);
             this.heliadsInCommon.TabIndex = 3;
             this.heliadsInCommon.Text = "label1";
             // 
             // plusLabel
             // 
             this.plusLabel.AutoSize = true;
-            this.plusLabel.Location = new System.Drawing.Point(128, 61);
+            this.plusLabel.Location = new System.Drawing.Point(192, 94);
+            this.plusLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.plusLabel.Name = "plusLabel";
-            this.plusLabel.Size = new System.Drawing.Size(13, 13);
+            this.plusLabel.Size = new System.Drawing.Size(18, 20);
             this.plusLabel.TabIndex = 2;
             this.plusLabel.Text = "+";
             // 
             // heliadaUpDown
             // 
-            this.heliadaUpDown.Location = new System.Drawing.Point(144, 58);
+            this.heliadaUpDown.Location = new System.Drawing.Point(216, 89);
+            this.heliadaUpDown.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.heliadaUpDown.Maximum = new decimal(new int[] {
             999,
             0,
@@ -193,7 +216,7 @@
             0,
             0});
             this.heliadaUpDown.Name = "heliadaUpDown";
-            this.heliadaUpDown.Size = new System.Drawing.Size(39, 20);
+            this.heliadaUpDown.Size = new System.Drawing.Size(58, 26);
             this.heliadaUpDown.TabIndex = 1;
             this.heliadaUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.heliadaUpDown.Value = new decimal(new int[] {
@@ -206,10 +229,11 @@
             // HeliadaDatePicker
             // 
             this.HeliadaDatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.HeliadaDatePicker.Location = new System.Drawing.Point(24, 58);
+            this.HeliadaDatePicker.Location = new System.Drawing.Point(36, 89);
+            this.HeliadaDatePicker.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.HeliadaDatePicker.MinDate = new System.DateTime(1996, 3, 1, 0, 0, 0, 0);
             this.HeliadaDatePicker.Name = "HeliadaDatePicker";
-            this.HeliadaDatePicker.Size = new System.Drawing.Size(100, 20);
+            this.HeliadaDatePicker.Size = new System.Drawing.Size(148, 26);
             this.HeliadaDatePicker.TabIndex = 0;
             this.HeliadaDatePicker.Value = new System.DateTime(2029, 12, 25, 23, 59, 59, 0);
             this.HeliadaDatePicker.ValueChanged += new System.EventHandler(this.heliadaDatePicker_ValueChanged);
@@ -218,26 +242,19 @@
             // 
             this.FormTooltip.ToolTipTitle = "Подсказка";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 27);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(286, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Подсчёт N-ой хелиады (1000 дней) для указанной даты";
-            // 
             // Form1
             // 
             this.AcceptButton = this.ToCommonButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(316, 194);
+            this.ClientSize = new System.Drawing.Size(474, 298);
             this.Controls.Add(this.TabControl);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
-            this.ShowIcon = false;
             this.Text = "Конвертер дат УЕХ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             this.TabControl.ResumeLayout(false);
             this.converterTab.ResumeLayout(false);
             this.converterTab.PerformLayout();
--- CalendarCalculator/Form1.cs
+++ CalendarCalculator/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CalendarCalculator.Properties;
+using Microsoft.Win32;
+using System;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
@@ -6,7 +8,12 @@ namespace CalendarCalculator
 {
     public partial class Form1 : Form
     {
+        // The path to the key where Windows looks for startup applications
+        RegistryKey rkAutostartApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
         private readonly DateConverterService converterService = new DateConverterService();
+        private NotifyIcon trayIcon;
+        MenuItem autostart;
         public Form1()
         {
             InitializeComponent();
@@ -18,6 +25,26 @@ namespace CalendarCalculator
 
             TutFormatInput.Text = tutToday;
             CommonFormatLabel.Text = converterService.ConvertToCommon(tutToday);
+
+            // Initialize Tray Icon
+            MenuItem showForm = new MenuItem("Открыть", showForm_Click);
+            MenuItem options = new MenuItem("Настройки");
+            autostart = new MenuItem("Автозапуск", autostart_Click);
+            autostart.Checked = rkAutostartApp.GetValue("CalendarCalculator") != null;
+            options.MenuItems.Add(autostart);
+            MenuItem exit = new MenuItem("Выход", exit_Click);
+
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resources.Icon,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    showForm,
+                    options,
+                    exit                    
+                }),
+                Visible = true
+            };
+            trayIcon.Text = tutToday;
         }
 
         private void DatePicker_ValueChanged(object sender, EventArgs e)
@@ -91,6 +118,49 @@ namespace CalendarCalculator
             DateTime resultDate = date.AddDays(1000 * heliadas);
             heliadsInCommon.Text = resultDate.ToString(DateConverterService.DATE_PATTERN);
             heliadsInTut.Text = converterService.ConvertToTUT(resultDate);
+        }
+
+        private void showForm_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        void exit_Click(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+
+        void autostart_Click(object sender, EventArgs e)
+        {
+            autostart.Checked = !autostart.Checked;
+            if (autostart.Checked)
+            {
+                rkAutostartApp.SetValue("CalendarCalculator", Application.ExecutablePath);
+            }
+            else
+            {
+                rkAutostartApp.DeleteValue("CalendarCalculator", false);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (trayIcon != null)
+            {
+                if (FormWindowState.Minimized == this.WindowState)
+                {
+                    this.Hide();
+                }
+            }
         }
     }
 }
